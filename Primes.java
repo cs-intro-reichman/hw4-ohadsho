c63d69507c2ab95e7a09cbe73c9fc4535b638ce7
Ohad Shoshani Levi <ohadsho34@gmail.com>
2024-11-28 19:15:18 +0200

    check

--- Primes.java
+++ Primes.java
@@ -1,7 +1,6 @@
 public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        System.out.println("Prime numbers up to "+ n+":");
         int i=0;
         int j=0;
         int primeCounter =0;
@@ -9,12 +8,15 @@ public class Primes {
         for(int k=2 ; k<n+1 ; k++){
             prime[k] = true;
         }
+        System.out.println("Prime numbers up to "+ n+":");
 
         while(i <= n){
             while(i <= n && prime[i] == false){
                 i++;
             }
-
+            if(i<n+1){
+            System.out.println(i);
+        }
             while(j<=n){
                 if(j%i == 0){
                     prime[j] = false;
@@ -26,8 +28,6 @@ public class Primes {
             primeCounter++;
         }
         primeCounter--;
-
-
         
         System.out.println("There are " + primeCounter+ " primes between 2 and " + n +" ("+ (int)(100.0/((double)n/(double)primeCounter)) +"%" +" are primes"+ ")");
     }
