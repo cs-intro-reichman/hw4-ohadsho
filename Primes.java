16fe8405df17fdb89223c99d00d718f45e3c1b60
Ohad Shoshani Levi <ohadsho34@gmail.com>
2024-11-28 18:21:30 +0200

    check

--- Primes.java
+++ Primes.java
@@ -1,5 +1,32 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        int i=0;
+        int j=0;
+        int primeCounter =0;
+        boolean[] prime = new boolean[n+1];
+        for(int k=2 ; k<n+1 ; k++){
+            prime[k] = true;
+        }
+
+        while(i <n){
+            while(i <= n && prime[i] == false){
+                i++;
+            }
+            System.out.println(i);
+            while(j<=n){
+                if(j%i == 0){
+                    prime[j] = false;
+                }
+                j++;
+            }
+            j=0;
+            primeCounter++;
+        }
+        primeCounter--;
+        
+        System.out.println("There are " + primeCounter+ " primes between 2 and " + n +" ("+ 100/(n/primeCounter) +"%" +" sare primes)");
     }
 }
+    
+
